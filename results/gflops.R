4e2586535dd1c3de82fe1f244f2ce6ca6ad39e48
Giovanni Azua Garcia <bravegag@Macintosh-4.local>
2013-08-12 15:19:11 +0200

    updating results

--- results/gflops.R
+++ results/gflops.R
@@ -29,18 +29,22 @@ basedir <- "/Users/bravegag/code/eigen-magma-benchmark/results/"
 dfa <- NULL
 dfa_mt <- NULL
 
+ylimit <- 200
+
+#impl <- "magma"
+impl <- "cublas"
+
 # read the relevant files
 #func_name <- "dgesvd"
-func_name <- "dpotrf"
+#func_name <- "dpotrf"
 #func_name <- "dtrsm"
 #func_name <- "dgemv"
 #func_name <- "dgeqp3"
-#func_name <- "dgemm"
+func_name <- "dgemm"
 
-file_names <- c(paste(func_name, '_eigen', sep=""), paste(func_name, '_mkl', sep=""), paste(func_name, '_magma', sep=""))
+file_names <- c(paste(func_name, '_eigen', sep=""), paste(func_name, '_mkl', sep=""), paste(func_name, paste('_', impl, sep=""), sep=""))
 #file_names <- c(paste(func_name, '_eigen', sep=""), paste(func_name, '_mkl', sep=""), paste(func_name, '_cublas', sep=""))
-labels <- c('eigen', 'mkl', 'magma')
-#labels <- c('eigen', 'mkl', 'cublas')
+labels <- c('eigen', 'mkl', impl)
 i <- 1
 for (f in file_names) {
 	all_files <- dir(path=basedir, pattern=paste(f, '\\.dat', sep=""))
@@ -72,23 +76,27 @@ dfa <- dfa[with(dfa, order(baseline, n)), ]
 
 df <- dfa
 df$main <- FALSE
-df$main[df$baseline == "magma"] <- TRUE
-#df$main[df$baseline == "cublas"] <- TRUE
+df$main[df$baseline == impl] <- TRUE
 
 # =========================================================================================
 # Gflops plot
 # =========================================================================================
 
+if (impl == 'magma') { 
+	colors <- c("grey20", "red", "darkgreen")
+} else {
+	colors <- c("red", "grey20", "darkgreen")	
+}
+
 # Draw the graph
 p <- ggplot(data=df, mapping=aes(x=n, y=gflops, colour=baseline)) +
-#scale_colour_manual(values=c("red", "grey20", "darkgreen"), guide="none") +
- scale_colour_manual(values=c("grey20", "red", "darkgreen"), guide="none") +
+ scale_colour_manual(values=colors, guide="none") +
  geom_path(aes(size=main),size=0.8) + geom_point(size=3.2) + 
  geom_path(data=df[df$main,],size=0.75,colour="red") + geom_point(data=df[df$main,],size=3.0,colour="red") +
  scale_size_discrete(range=c(0.5,1), guide="none") +
  scale_x_continuous(expand=c(0,0), breaks=seq(0,10000,by=1000), limit=c(0,max(df$n + 1500))) +
  geom_hline(aes(yintercept=0)) +
- scale_y_continuous("[Gflop/s]", limit=c(0,150), expand=c(0,0)) + 
+ scale_y_continuous("[Gflop/s]", limit=c(0,ylimit), expand=c(0,0)) + 
  labs(title=paste("Gflops ", func_name, ", Xeon E5-2690, nVidia GTX Titan", sep=""))
 
 p <- direct.label(p, list("last.qp",vjust=0.3,hjust=-0.2,fontfamily="sans",fontsize=15,fontface="plain"))
